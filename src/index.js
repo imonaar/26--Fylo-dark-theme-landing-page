75dea55219186166bf82b8e98b854adb44b11520
imonaar <kevinmonari22@gmail.com>
2021-09-15 17:34:15 +0300

    final touches

--- src/index.js
+++ src/index.js
@@ -1,3 +1,41 @@
 import "./styles/styles.scss";
 
 console.log("hello world!");
+
+const form = document.forms['form']
+const email = document.forms['form']['email']
+const error = document.querySelector('span.error')
+
+email.addEventListener("input", (event) => {
+  if(email.validity.valid){
+    
+    error.textContent = ""
+
+  } else{
+    showError()
+  }
+});
+
+form.addEventListener('submit', (event)=>{
+  if(!email.validity.valid){
+    showError()
+
+    event.preventDefault()
+  }
+})
+
+function showError(){
+  if(email.validity.valueMissing){
+
+    error.textContent = 'You need to enter an e-mail address.';
+
+  } else if(email.validity.typeMismatch){
+
+    error.textContent ="Oops! That doesn’t look like an email address"
+
+  } else if(email.validity.tooShort){
+
+    error.textContent =`Email should be at least ${ email.minLength } characters; you entered ${ email.value.length }.`;
+
+  }
+}
